21b156a4107e3c46c441f81978cb6b9f6db7d2fc
watanabe-cit <s2242129hq@s.chibakoudai.jp>
2025-07-03 13:31:33 +0900

    a

--- htdocs/test_db.php
+++ htdocs/test_db.php
@@ -1,13 +1,13 @@
 <?php
-$host = '127.0.0.1';
-$db   = 'shiritori';
+$host = 'localhost';
+$db   = 'testuser_db';  // ← ここもあなたのDB名に
 $user = 'testuser';
 $pass = 'pass';
 
 try {
   $pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8", $user, $pass);
-  echo "✅ 接続成功しました！";
+  echo "✅ 接続成功！";
 } catch (PDOException $e) {
-  echo "❌ 接続失敗: " . $e->getMessage();
+  echo "❌ 接続失敗：" . $e->getMessage();
 }
 ?>
