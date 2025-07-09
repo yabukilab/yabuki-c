8f7021d6dae1153341cb7f21ea267e1509baf0a4
watanabe-cit <s2242129hq@s.chibakoudai.jp>
2025-07-09 07:52:14 +0000

    アップ漏れ

--- htdocs/db.php
+++ htdocs/db.php
@@ -25,3 +25,8 @@ try {
 } catch (PDOException $e) {
   echo "Can't connect to the database: " . h($e->getMessage());
 }
+
+if (password_verify("admin123",'$2y$10$OKU8CGvFF0v3hjCEcdR7VuVeK8xvH/N2XzxlQ6VnZ4pYvT8goNmie')){
+  echo "hell";
+}
+echo password_hash('admin123', PASSWORD_DEFAULT);
