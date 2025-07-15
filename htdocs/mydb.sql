7c50688d4627380fea781075e404b0e06bde4214
渡邊悠介 <s2242129hq@s.chibakoudai.jp>
2025-07-15 08:01:50 +0000

    データの入力

--- htdocs/mydb.sql
+++ htdocs/mydb.sql
@@ -1,5 +1,5 @@
-INSERT INTO users (username, password, is_admin) VALUES ('user2', '$2y$10$Gu6ua7webiYDONHErtMzce7xAC9bm9R24IAGsk3AGnVSculI0vuOW', 0);
-INSERT INTO users (username, password, is_admin) VALUES ('admin', '$2y$10$nnAT4UCH2HHuGb1ZK.UzhunbAAHzD/Uh5P4/WoPnLy8ViZUGZ88Ua', 0);
-INSERT INTO users (username, password, is_admin) VALUES ('渡邊悠介', '$2y$10$wI8XMDqkvXCXGj.nZKKsael34LfeVNgbHeayPbm9qixWLSFbhV5Ke', 0);
-INSERT INTO users (username, password, is_admin) VALUES ('user3', '$2y$10$aPmA4I87a34ndO2yGrQEo.UZr1.hBYPAE2kOXaFjEqdIuTWOAxebq', 0);
-INSERT INTO users (username, password, is_admin) VALUES ('user1', '$2y$10$5XgPT9iDDPsd4Tl7oFXGo.RwO4/AI3iMUiMJdUFHGkRhnNm1ywea6', 0);
+INSERT INTO users (username, password, is_admin, best_score, best_time) VALUES ('user2', '$2y$10$Gu6ua7webiYDONHErtMzce7xAC9bm9R24IAGsk3AGnVSculI0vuOW', 0, 60, 60);
+INSERT INTO users (username, password, is_admin, best_score, best_time) VALUES ('admin', '$2y$10$nnAT4UCH2HHuGb1ZK.UzhunbAAHzD/Uh5P4/WoPnLy8ViZUGZ88Ua', 0, 12, 60);
+INSERT INTO users (username, password, is_admin, best_score, best_time) VALUES ('渡邊悠介', '$2y$10$wI8XMDqkvXCXGj.nZKKsael34LfeVNgbHeayPbm9qixWLSFbhV5Ke', 0);
+INSERT INTO users (username, password, is_admin, best_score, best_time) VALUES ('user3', '$2y$10$aPmA4I87a34ndO2yGrQEo.UZr1.hBYPAE2kOXaFjEqdIuTWOAxebq', 0);
+INSERT INTO users (username, password, is_admin, best_score, best_time) VALUES ('user1', '$2y$10$5XgPT9iDDPsd4Tl7oFXGo.RwO4/AI3iMUiMJdUFHGkRhnNm1ywea6', 0);
