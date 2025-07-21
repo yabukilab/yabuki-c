5345e699e7ddbd4a7f1d1621cd832aa65efac07c
渡邊悠介 <s2242129hq@s.chibakoudai.jp>
2025-07-21 14:01:24 +0000

    db

--- htdocs/mydb.sql
+++ htdocs/mydb.sql
@@ -1,1 +1,2 @@
 INSERT INTO users (username, password, is_admin) VALUES ('pass1', '$2y$10$K8GpK0q1QqjmjFnoL0piK.QX3mBhviQuR43F7vW6PlSfmOHItU7XK', 0);
+INSERT INTO users (username, password, is_admin) VALUES ('user1', '$2y$10$n2/vZmNP588b2Nh2Mco6q.rLT6NPJN4BkQzJKiC98Veq6WC2CNyNK', 0);
