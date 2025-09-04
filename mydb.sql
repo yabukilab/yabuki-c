b12cd71d3fbbe94a8367375065df1a0ef1265f28
meguroyuusei <s2342127GZ@s.chibakoudai.jp>
2025-09-04 15:07:54 +0900

    同じスコアの記録が一つしか残らない現象を修正

--- mydb.sql
+++ mydb.sql
@@ -1,30 +1,39 @@
 -- phpMyAdmin SQL Dump
 -- version 5.2.1deb3
 -- https://www.phpmyadmin.net/
+--
+-- ホスト: localhost:3306
+-- サーバのバージョン： 10.11.11-MariaDB-0ubuntu0.24.04.2
+-- PHP のバージョン: 8.3.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
-CREATE DATABASE IF NOT EXISTS mydb DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+CREATE DATABASE IF NOT EXISTS mydb CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 USE mydb;
 
--- -----------------------------
--- テーブル: users
--- -----------------------------
-CREATE TABLE IF NOT EXISTS `users` (
+-- --------------------------------------------------------
+
+-- ユーザーテーブル
+CREATE TABLE `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   `is_admin` tinyint(1) DEFAULT 0,
   PRIMARY KEY (`id`),
   UNIQUE KEY `username` (`username`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- -----------------------------
--- テーブル: score
--- -----------------------------
-CREATE TABLE IF NOT EXISTS `score` (
+-- 初期ユーザー
+INSERT INTO `users` (`id`, `username`, `password`, `is_admin`) VALUES
+(1, 'user1', '$2y$10$JUVEU6xv5GAHbgXIt7.8Q.N6ciSNdAFCyzwS7PHlanUqr1Phcn1La', 0)
+ON DUPLICATE KEY UPDATE username=username;
+
+-- --------------------------------------------------------
+
+-- スコアテーブル
+CREATE TABLE `score` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `user_id` int(11) NOT NULL,
   `score` int(11) NOT NULL,
@@ -33,6 +42,6 @@ CREATE TABLE IF NOT EXISTS `score` (
   PRIMARY KEY (`id`),
   KEY `user_id` (`user_id`),
   CONSTRAINT `score_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 COMMIT;
